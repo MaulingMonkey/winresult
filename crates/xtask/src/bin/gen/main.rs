f8dc4223d6ec1ab02c9c485868b5f57c2e830e47
MaulingMonkey <git@maulingmonkey.com>
2022-02-18 11:42:05 -0800

    xtask gen: prep for more headers

--- crates/xtask/src/bin/gen/main.rs
+++ crates/xtask/src/bin/gen/main.rs
@@ -54,22 +54,57 @@ fn main() {
     )*}}
 
     headers! {
-        d3d9_h          => r"shared\d3d9.h",
         ntstatus_h      => r"shared\ntstatus.h",
         winerror_h      => r"shared\winerror.h",
+        // already covered by winerror.h:
+        //  um\fltWinError.h
+        //  um\Msi.h
 
+        d3d9_h          => r"shared\d3d9.h",
         d3d_h           => r"um\d3d.h",
         d3d9helper_h    => r"um\d3d9helper.h",
         d3dhal_h        => r"um\d3dhal.h",
+
+        // TODO:
+        //  dhcpsapi_h      => r"um\dhcpsapi.h",
+        //  mprerror_h      => r"um\MprError.h",
+        //  netsh_h         => r"um\NetSh.h",
+        //  patchapi_h      => r"um\PatchApi.h",
+        //  patchwiz_h      => r"um\PatchWiz.h",
+        //  raserror_h      => r"um\RasError.h",
+        //  setupapi_h      => r"um\SetupAPI.h",
+        //  tcerror_h       => r"um\TCError.h",
+        //  winhttp_h       => r"um\winhttp.h",
+        //  wininet_h       => r"um\WinInet.h",
+        //  winineti_h      => r"um\Winineti.h",
+        //  winioctl_h      => r"um\winioctl.h",
+        //  winspool_h      => r"um\winspool.h",
+        //  wsmerror        => r"um\wsmerror.h",
     }
 
     let mut codes   = scan::Codes::default();
+    scan::ntstatus_h    (&ntstatus_h,   &mut codes);
     scan::winerror_h    (&winerror_h,   &mut codes);
+
     scan::d3d           (&d3d9_h,       &mut codes);
     scan::d3d           (&d3d_h,        &mut codes);
     scan::d3d           (&d3d9helper_h, &mut codes);
     scan::d3d           (&d3dhal_h,     &mut codes);
-    scan::ntstatus_h    (&ntstatus_h,   &mut codes);
+
+    //scan::winerror_h    (&dhcpsapi_h,   &mut codes);
+    //scan::winerror_h    (&mprerror_h,   &mut codes);
+    //scan::winerror_h    (&netsh_h,      &mut codes);
+    //scan::winerror_h    (&patchapi_h,   &mut codes);
+    //scan::winerror_h    (&patchwiz_h,   &mut codes);
+    //scan::winerror_h    (&raserror_h,   &mut codes);
+    //scan::winerror_h    (&setupapi_h,   &mut codes);
+    //scan::winerror_h    (&tcerror_h,    &mut codes);
+    //scan::winerror_h    (&winhttp_h,    &mut codes);
+    //scan::winerror_h    (&wininet_h,    &mut codes);
+    //scan::winerror_h    (&winineti_h,   &mut codes);
+    //scan::winerror_h    (&winioctl_h,   &mut codes);
+    //scan::winerror_h    (&winspool_h,   &mut codes);
+    //scan::winerror_h    (&wsmerror,     &mut codes);
 
     gen::codes(&codes);
     gen::readme();
